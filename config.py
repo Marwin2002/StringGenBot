7f1359c83f3063492e63f2d72aa08b9b647b1f31
Marwin2002 <musicbotid@gmail.com>
2024-11-22 10:47:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("27758016"))
+API_HASH = getenv("8d34cfffe27ab461eabbf0091b1a27df")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 7224419362))
