848635469a4efc9391bd2065366f65c5916a1cbe
Marwin2002 <musicbotid@gmail.com>
2024-11-22 10:55:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("27758016"))
-API_HASH = getenv("8d34cfffe27ab461eabbf0091b1a27df")
+API_ID = "27758016"
+API_HASH = "8d34cfffe27ab461eabbf0091b1a27df"
 
-BOT_TOKEN = getenv("7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://marwin0985:BEwJvxaADStDLScc@cluster0.oh0nk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = "7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs"
+MONGO_DB_URI = "mongodb+srv://marwin0985:BEwJvxaADStDLScc@cluster0.oh0nk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-OWNER_ID = int(getenv("OWNER_ID", "7224419362"))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Architect04")
+OWNER_ID = "7224419362"
+SUPPORT_CHAT = "https://t.me/The_Architect04"
