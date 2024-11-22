9471dfc1a9d22439ff3b63e9fcd4b58a71c31bdb
Marwin2002 <musicbotid@gmail.com>
2024-11-22 10:50:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("27758016"))
 API_HASH = getenv("8d34cfffe27ab461eabbf0091b1a27df")
 
 BOT_TOKEN = getenv("7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://marwin0985:BEwJvxaADStDLScc@cluster0.oh0nk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", 7224419362))
+OWNER_ID = int(getenv("OWNER_ID", "7224419362"))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Architect04")
