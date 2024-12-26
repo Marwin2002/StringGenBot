e8dec7766e9c1686907ed2b5864ea56c7394188d
Marwin2002 <musicbotid@gmail.com>
2024-12-26 13:02:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = "27758016"
 API_HASH = "8d34cfffe27ab461eabbf0091b1a27df"
 
-BOT_TOKEN = "7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs
+BOT_TOKEN = "7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs"
 MONGO_DB_URI = "mongodb+srv://marwin0985:BEwJvxaADStDLScc@cluster0.oh0nk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 OWNER_ID = "7224419362"
